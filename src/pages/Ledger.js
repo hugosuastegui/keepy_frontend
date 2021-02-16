834d380a50f311df9de2b8e0388507722299dd35
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2021-02-15 19:01:57 -0600

    Legder adds concepts on frontend, backeng intgration pending, balance calc pending

--- src/pages/Ledger.js
+++ src/pages/Ledger.js
@@ -41,59 +41,82 @@ function Ledger() {
   }, [project]);
 
   const addConcept = (values) => {
-    console.log(values);
+    const newConcept = {
+      description: values.description,
+      day: values.date.format("DD"),
+      month: values.date.format("MMM"),
+      year: values.date.format("YYYY"),
+      amount: values.amount,
+      subaccount: values.subaccount,
+    };
+    setConcepts([...concepts, newConcept]);
+    form.resetFields();
   };
 
   return user ? (
     <div>
       <Title>Ledger</Title>
-      {project ? (
-        <>
-          {project.subaccounts.length === 0 ? (
-            <p>
-              Before you can add any concept yo have to first add Subaccounts
-            </p>
-          ) : (
-            <></>
-          )}
-          <Button type="default">
-            <Link to={`/subaccounts`}>Subaccounts</Link>
-          </Button>
-          <br />
-          <br />
-
-          <Form
-            name="concept-form"
-            form={form}
-            layout="inline"
-            onFinish={addConcept}
-          >
-            <Form.Item name="description" label="Concept">
-              <Input size="large" placeholder="Description"></Input>
-            </Form.Item>
-            <Form.Item name="date">
-              <DatePicker />
-            </Form.Item>
-            <Form.Item name="amount">
-              <InputNumber size="medium" placeholder="Amount"></InputNumber>
-            </Form.Item>
-            <Form.Item name="subaccount">
-              <Select placeholder="Subaccount">
-                {subaccountItems.map((subaccount, ind) => (
-                  <Option key={ind}>{subaccount.name}</Option>
-                ))}
-              </Select>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </>
-      ) : (
-        <p>First you have to select a project from the Projects Menu</p>
-      )}
+      <Button type="default">
+        <Link to={`/subaccounts`}>Subaccounts</Link>
+      </Button>
+      <br />
+      <br />
+      <div className="ledgerContent">
+        {project ? (
+          <>
+            {project.subaccounts.length === 0 ? (
+              <p>
+                Before you can add any concept yo have to first add Subaccounts
+              </p>
+            ) : (
+              <></>
+            )}
+            <Form
+              name="concept-form"
+              form={form}
+              layout="inline"
+              onFinish={addConcept}
+            >
+              <Form.Item name="description" label="Concept">
+                <Input size="medium" placeholder="Description"></Input>
+              </Form.Item>
+              <Form.Item name="date">
+                <DatePicker />
+              </Form.Item>
+              <Form.Item name="amount">
+                <Input size="medium" placeholder="Amount"></Input>
+              </Form.Item>
+              <Form.Item name="subaccount">
+                <Select placeholder="Subaccount">
+                  {subaccountItems.map((subaccount, ind) => (
+                    <Option key={subaccount.name}>{subaccount.name}</Option>
+                  ))}
+                </Select>
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+            <br />
+            <br />
+          </>
+        ) : (
+          <p>First you have to select a project from the Projects Menu</p>
+        )}
+        {concepts.map((concept, id) => (
+          <div className="concept">
+            <div>{concept.subaccount}</div>
+            <div>{concept.day}</div>
+            <div>{concept.month}</div>
+            <div>{concept.year}</div>
+            <div>{concept.description}</div>
+            <div>{concept.amount}</div>
+            <div>Balance</div>
+          </div>
+        ))}
+      </div>
     </div>
   ) : (
     <Redirect to="/login"></Redirect>
